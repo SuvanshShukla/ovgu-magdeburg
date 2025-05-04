b604fa5c0af5da4f115db8b04db93b3b341e76fa
Suvansh Shukla <suvanshshukla@gmail.com>
2025-05-04 11:39:51 +0200

    update implement missed methods

--- 03_ISEE/02_assignments/Monster.java
+++ 03_ISEE/02_assignments/Monster.java
@@ -4,10 +4,64 @@
 public class Monster implements IMonster{
 
     String name;
+    float maxHp;
     float hp;
     float weight;
     float mult;
     int attack;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public float getHealth() {
+        return hp;
+    }
+
+    @Override
+    public float getMaxHealth() {
+        return maxHp;
+    }
+
+    /**
+     * Checks if hp value is greater than 0
+     * if greater than 0 then monster is still alive (true)
+     * otherwise it is dead (false).
+     * @return boolean
+     */
+    @Override
+    public boolean isAlive() {
+        return hp > 0;
+    }
+
+    @Override
+    public float getBaseAttack() {
+        return attack;
+    }
+
+    @Override
+    public int getAttack() {
+        return attack;
+    }
+
+    @Override
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * This method sets the multiplier with the condition its value
+     * must be between -2.0 & 5.0
+     * @param multiplier float
+     */
+    @Override
+    public void setAttackMultiplier(float multiplier) {
+        if (multiplier >= -2.0f && multiplier <= 5.0f) {
+            mult = multiplier;
+        }
+    }
 
     /**
      * Reduce hp by damage taken.
@@ -38,12 +92,12 @@ public class Monster implements IMonster{
     /**
      * Constructs an object of the monster class
      * @param name name of the monster
-     * @param hp health points of the monster, cannot exceed 1
+     * @param maxHp max health points of the monster, cannot be less than 1
      * @param weight weight of the monster in kg, must be at least 100g
      * @param attack attack value, must be an int
      * @param mult attack value multiplier, must be between -2.0 and 5.0
      */
-    public Monster(String name, float hp, float weight, int attack, float mult) {
+    public Monster(String name, float maxHp, float weight, int attack, float mult) {
 
         if (weight <0.1) {
             this.weight = 0.1f;
@@ -51,15 +105,18 @@ public class Monster implements IMonster{
             this.weight = weight;
         }
 
-        if(hp > 1) {
-            this.hp = 1;
+        if (maxHp < 1) {
+            this.maxHp = 1;
         } else {
-            this.hp = hp;
+            this.maxHp = maxHp;
+        }
+
+        if (mult >= -2.0f && mult <= 5.0f) {
+            this.mult = mult;
         }
 
         this.name = name;
         this.attack = attack;
-        this.mult = mult;
     }
 }
 
