f188c3973f85fd8164887af611b3372260f33120
Suvansh Shukla <suvanshshukla@gmail.com>
2025-06-14 16:54:35 +0200

    update to be inline with coding guidelines

--- 03_ISEE/02_assignments/exercise7/RockPaperScissors.java
+++ 03_ISEE/02_assignments/exercise7/RockPaperScissors.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
   public static void main(String[] args) {
+
     Scanner input = new Scanner(System.in);
     System.out.println("Rock-paper-scissors");
     System.out.println("===================");
@@ -10,102 +11,113 @@ public class RockPaperScissors {
     System.out.println("(1) Scissors");
     System.out.println("(2) Rock");
     System.out.println("(3) Paper");
+
     int choice = 0;
     while (choice < 1 || choice > 3) {
       System.out.print("Input:");
       choice = input.nextInt();
     }
+    input.close();
+
     System.out.print("You chose ");
-    switch (choice) {
-      case 1: {
-        System.out.print("scissors");
-      }
-        break;
-      case 2: {
-        System.out.print("rock");
-      }
-        break;
-      case 3: {
-        System.out.print("paper");
-      }
-        break;
-    }
-    System.out.println(" as weapon.");
+
+    printChoice(choice);
+
     Random random = new Random();
     int enemy = random.nextInt(3) + 1;
+
     System.out.print("Your opponent chose ");
-    switch (enemy) {
+
+    printChoice(enemy);
+
+    printOutcome(choice, enemy);
+
+  }
+
+  public static void printChoice(int choice) {
+    switch (choice) {
       case 1: {
         System.out.print("scissors");
       }
-        break;
+      break;
       case 2: {
         System.out.print("rock");
       }
-        break;
+      break;
       case 3: {
         System.out.print("paper");
       }
-        break;
+      break;
     }
+
     System.out.println(" as wepaon.");
+  }
+
+  public static void printOutcome(int choice, int enemy) {
+
+    if (choice == enemy) {
+      System.out.println("Draw!");
+      return;
+    } else {
+      if(choice == 1 && enemy == 2) {
+        System.out.println("You Win!");
+      } else if (choice == 2 && enemy == 3) {
+      }
+    }
+
     switch (choice) {
       case 1: {
         if (enemy == 1) {
           System.out.println("Draw!");
-          input.close();
           return;
         }
+
         if (enemy == 2) {
           System.out.println("You lose!");
-          input.close();
           return;
         }
+
         if (enemy == 3) {
           System.out.println("You win!");
-          input.close();
           return;
         }
       }
-        break;
+      break;
       case 2: {
+
         if (enemy == 1) {
           System.out.println("You win!");
-          input.close();
           return;
         }
+
         if (enemy == 2) {
           System.out.println("Draw!");
-          input.close();
           return;
         }
+
         if (enemy == 3) {
           System.out.println("You lose!");
-          input.close();
           return;
         }
       }
-        break;
+      break;
       case 3: {
         if (enemy == 1) {
           System.out.println("You lose!");
-          input.close();
           return;
         }
+
         if (enemy == 2) {
           System.out.println("You win!");
-          input.close();
           return;
         }
+
         if (enemy == 3) {
           System.out.println("Draw!");
-          input.close();
           return;
         }
       }
-        break;
+      break;
     }
-    System.out.println("ERROR: The program should never arrive here!");
-    input.close();
   }
 }
