aa5c9515fffd663e4499b436eaf784d77870aee0
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-23 08:57:44 +0200

    update: fix logic

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
@@ -1,20 +1,20 @@
 def is_prime(n: int) -> bool:
     """Returns `True`, if and only if `n` is prime."""
-    for i in range (2, n+1):
-        if (n%i !=0 and n==i):
-            return True
-    return False
+    for i in range (2, n):
+        if n%i==0 and i<n:
+            return False
+    return True
     raise NotImplementedError()
 
 
 def next_prime(n: int) -> int:
     """Returns the next prime number that is bigger or equal to `n`."""
     j = n+1
+    i = 2
     while not is_prime(j):
-        j += 1
+        j +=1
     return j
     raise NotImplementedError()
-
 
 if __name__ == "__main__":
     pass
