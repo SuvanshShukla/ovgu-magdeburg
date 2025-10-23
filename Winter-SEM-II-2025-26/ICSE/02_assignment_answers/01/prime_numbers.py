727632abdaa00ab8af94feb960e71b3c87e223ab
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-23 10:27:41 +0200

    update: modify slightly for linting

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
@@ -2,7 +2,7 @@ def is_prime(n: int) -> bool:
     """Returns `True`, if and only if `n` is prime."""
     if n == 0 or n == 1:
         return False
-        
+    
     for i in range(2, n):
         if n % i == 0 and i < n:
             return False
