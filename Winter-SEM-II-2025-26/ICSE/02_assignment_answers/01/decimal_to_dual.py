5bb9c2ebb1658dc95865085cde378dc6d1ba24b0
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-23 09:41:17 +0200

    add: decimal_to_dual.py

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/decimal_to_dual.py
@@ -0,0 +1,7 @@
+def transform_to_dual(dec: int) -> str:
+    """Transforms a given decimal number to it's dual representation."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    pass
