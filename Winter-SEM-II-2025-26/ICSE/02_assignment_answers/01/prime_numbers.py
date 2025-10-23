831ecb1f17e5b474155bb6fb2e092e21a064004b
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-23 09:12:54 +0200

    update: logic to include number itself in next_prime and update to statisfy linter

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/prime_numbers.py
@@ -1,23 +1,20 @@
 def is_prime(n: int) -> bool:
     """Returns `True`, if and only if `n` is prime."""
-    if n == 0 or n== 1:
-        return True
-
+    if n == 0 or n == 1:
+        return False
+        
     for i in range (2, n):
-        if n%i==0 and i<n:
+        if n % i == 0 and i < n:
             return False
-
     return True
     raise NotImplementedError()
 
 
 def next_prime(n: int) -> int:
     """Returns the next prime number that is bigger or equal to `n`."""
-    j = n+1
-    i = 2
-    while not is_prime(j):
-        j +=1
-    return j
+    while not is_prime(n):
+        n += 1
+    return n
     raise NotImplementedError()
 
 if __name__ == "__main__":
