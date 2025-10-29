592a71fe24b037c8b6c57cff37d7c160e591d8e8
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-29 11:34:48 +0100

    update: formatting fix

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/median_of_three.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/01/median_of_three.py
@@ -1,18 +1,18 @@
 def median(a: int, b: int, c: int) -> int:
     if (a <= b and b <= c) or (c <= b and b <= a):
-        return b 
+        return b
     if (b <= a and a <= c) or (c <= a and a <= b):
-        return a 
+        return a
     if (b <= c and c <= a) or (a <= c and c <= b):
         return c
-    raise NotImplementedError()  
+    raise NotImplementedError()
 
 
 def median2(a: int, b: int, c: int) -> int:
     nums = [a, b, c]
     nums.sort()
     return nums[1]
-    raise NotImplementedError() 
+    raise NotImplementedError()
 
 
 def test_median():
@@ -21,8 +21,8 @@ def test_median():
     assert median(77, 99, 23) == 77
     assert median(99, 99, 99) == 99
     assert median2(109, 108, 110) == 109
-    
-    
+
+   
 if __name__ == "__main__":
     print(median(0, 0, 0))
     test_median()
