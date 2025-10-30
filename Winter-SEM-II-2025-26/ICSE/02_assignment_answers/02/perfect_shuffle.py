5578e0df9a1efb19fb68b89def56d5f2aa5ca915
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-30 17:10:22 +0100

    update: solve task 2

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/02/perfect_shuffle.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/02/perfect_shuffle.py
@@ -1,20 +1,32 @@
 def interleave(a: list[int], b: list[int]) -> list[int]:
-    """
-    Returns a new array with elements consecutively taken from array `a` and
-    `b`.
-    """
-    raise NotImplementedError()  # TODO: Add implementation
+    shuffled_deck = []
+    for i in range(len(a)):
+        shuffled_deck.append(a[i])
+        shuffled_deck.append(b[i])
+    return shuffled_deck
 
 
 def perfect_shuffle(a: list[int]) -> list[int]:
-    """Returns a new array that is perfectly shuffled once."""
-    raise NotImplementedError()  # TODO: Add implementation
+    mid_ptr = int(len(a) / 2)
+    l_half = a[0:mid_ptr]
+    r_half = a[mid_ptr:len(a)]
+    return interleave(l_half, r_half)
 
 
 def shuffle_number(a: int) -> int:
-    """Returns the number of perfect shuffles needed to get to the same array."""
-    raise NotImplementedError()  # TODO: Add implementation
+    count = 0
+    original = list(range(a))
+    copy = original.copy()
+    while True:
+        copy = perfect_shuffle(copy)
+        count += 1
+        if original == copy:
+            break
+    return count
 
 
 if __name__ == "__main__":
+    assert shuffle_number(4) == 2
+    assert shuffle_number(52) == 8
+    print("ALL TESTS PASSED!")
     pass
