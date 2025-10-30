2d8395c2754926ef2247678b81460b43567485a9
SuvanshShukla <suvanshshukla@gmail.com>
2025-10-30 16:14:04 +0100

    add: with script template code

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/02/perfect_shuffle.py
@@ -0,0 +1,20 @@
+def interleave(a: list[int], b: list[int]) -> list[int]:
+    """
+    Returns a new array with elements consecutively taken from array `a` and
+    `b`.
+    """
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def perfect_shuffle(a: list[int]) -> list[int]:
+    """Returns a new array that is perfectly shuffled once."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def shuffle_number(a: int) -> int:
+    """Returns the number of perfect shuffles needed to get to the same array."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    pass
