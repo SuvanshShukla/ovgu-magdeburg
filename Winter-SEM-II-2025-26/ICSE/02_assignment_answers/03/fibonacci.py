84b90d97aa876f1739e5c10aca01e0c76ba7861d
Suvansh Shukla <suvanshshukla@gmail.com>
2025-11-03 08:16:11 +0100

    add fibonacci.py

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/fibonacci.py
@@ -0,0 +1,21 @@
+"""
+Solution for part 2.
+# TODO
+"""
+
+number_recursive_calls = 0
+
+
+def fib1(n: int) -> int:
+    """Calculates the `n`th fibonacci number recursively."""
+    global number_recursive_calls
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def fib2(n: int) -> int:
+    """Calculates the `n`th fibonacci number iteratively."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    pass
