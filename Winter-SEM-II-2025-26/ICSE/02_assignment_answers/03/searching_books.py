abd1d154c3767fb38edc0e01aab85c2e6040bbca
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-04 20:55:33 +0100

    add working linear & binary search

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/searching_books.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/searching_books.py
@@ -1,22 +1,65 @@
 def search_linear(a: list[str], item: str) -> int | None:
     """Searches the list for item linearly. Returns the position of item."""
-    raise NotImplementedError()  # TODO: Add implementation
+    for i in (a):
+        if a[i] == item:
+            return i
+        else:
+            return None
 
 
 def search_binary(a: list[str], item: str) -> int | None:
     """Searches the list for item binary. Returns the position of item."""
-    raise NotImplementedError()  # TODO: Add implementation
+    if len(a) == 0:
+        return None
+    elif len(a) == 1:
+        return a[0]
+    else:
+        median = int(len(a) / 2)
+        if a[median] == item:
+            return median
+        elif item < a[median]:
+            search_binary(a[0:median], item)
+        elif item >= a[median]:
+            search_binary(a[median+1:len(a) - 1], item)
 
 
 def search_linear_cmp_count(a: list[str], item: str) -> int:
-    """Searches the list for item linearly. Returns the number of comparisions needed."""
-    raise NotImplementedError()  # TODO: Add implementation
+    cmp = 0
+    for i in (a):
+        cmp += 1
+        if a[i] == item:
+            return cmp
+        else:
+            return None
 
 
 def search_binary_cmp_count(a: list[str], item: str) -> int:
-    """Searches the list for item linearly. Returns the number of comparisions needed."""
-    raise NotImplementedError()  # TODO: Add implementation
+    cmp = 0
+    if len(a) == 0:
+        cmp += 1
+        return cmp
+    elif len(a) == 1:
+        cmp += 1
+        return cmp
+    else:
+        median = int(len(a) / 2)
+        if a[median] == item:
+            cmp += 1
+            return cmp
+        elif item <= a[median]:
+            cmp += 1
+            search_binary(a[0:median], item)
+        elif item > a[median]:
+            cmp += 1
+            search_binary(a[median+1, len(a) - 1])
+
+
+def test_search():
+    assert search_linear([1, 9, 3, 2, 1, 5], 9) == 1
+    assert search_binary(range(20), 10) == 10
 
 
 if __name__ == "__main__":
+    test_search()
+    print("ALL TEST CASED PASSED!")
     pass
