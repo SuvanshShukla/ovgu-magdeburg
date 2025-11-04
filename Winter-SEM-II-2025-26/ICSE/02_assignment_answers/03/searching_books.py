e2021a58281b3c7be8f43d538fc69e92b0c397e0
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-04 20:30:00 +0100

    add searching books template

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/searching_books.py
@@ -0,0 +1,22 @@
+def search_linear(a: list[str], item: str) -> int | None:
+    """Searches the list for item linearly. Returns the position of item."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def search_binary(a: list[str], item: str) -> int | None:
+    """Searches the list for item binary. Returns the position of item."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def search_linear_cmp_count(a: list[str], item: str) -> int:
+    """Searches the list for item linearly. Returns the number of comparisions needed."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def search_binary_cmp_count(a: list[str], item: str) -> int:
+    """Searches the list for item linearly. Returns the number of comparisions needed."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    pass
