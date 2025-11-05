32023872f16ef4e395e2608be9dce48f4907a787
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-05 07:46:21 +0100

    completely fixed and working binary search

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/searching_books.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/searching_books.py
@@ -1,62 +1,72 @@
 def search_linear(a: list[str], item: str) -> int | None:
     """Searches the list for item linearly. Returns the position of item."""
-    for i in (a):
-        if a[i] == item:
-            return i
-        else:
-            return None
+    for i in a:
+        if i == item:
+            return a.index(i)
+    return None
 
 
 def search_binary(a: list[str], item: str) -> int | None:
     """Searches the list for item binary. Returns the position of item."""
-    if len(a) == 0:
+    temp_arr = a
+    offset = 0
+    if len(temp_arr) == 0:
         return None
-    elif len(a) == 1:
-        return a[0]
-    else:
-        median = int(len(a) / 2)
-        if a[median] == item:
-            return median
-        elif item < a[median]:
-            search_binary(a[0:median], item)
-        elif item >= a[median]:
-            search_binary(a[median+1:len(a) - 1], item)
+    elif len(temp_arr) == 1 and temp_arr[0] == item:
+        return 0
+    while temp_arr:
+        median = len(temp_arr) // 2
+        mid_item = temp_arr[median]
+        if mid_item == item:
+            return offset + median
+        elif item < mid_item:
+            temp_arr = temp_arr[:median]
+        elif item >= mid_item:
+            offset += median + 1
+            temp_arr = temp_arr[median + 1:]
+    return None
 
 
 def search_linear_cmp_count(a: list[str], item: str) -> int:
     cmp = 0
-    for i in (a):
-        cmp += 1
-        if a[i] == item:
+    for i in a:
+        if i == item:
+            cmp += 1
             return cmp
-        else:
-            return None
+    return cmp
 
 
 def search_binary_cmp_count(a: list[str], item: str) -> int:
     cmp = 0
-    if len(a) == 0:
+    temp_arr = a
+    offset = 0
+    if len(temp_arr) == 0:
         cmp += 1
         return cmp
-    elif len(a) == 1:
-        cmp += 1
+    elif len(temp_arr) == 1 and temp_arr[0] == item:
+        cmp += 2
         return cmp
-    else:
-        median = int(len(a) / 2)
-        if a[median] == item:
+    while len(temp_arr) > 1:
+        median = len(temp_arr) // 2
+        mid_item = temp_arr[median]
+        if mid_item == item:
             cmp += 1
+            return 0
+        elif item < mid_item:
+            cmp += 1
+            temp_arr = temp_arr[:median]
+        elif item >= mid_item:
+            cmp += 1
+            offset += median + 1
+            temp_arr = temp_arr[median + 1:]
+        else:
             return cmp
-        elif item <= a[median]:
-            cmp += 1
-            search_binary(a[0:median], item)
-        elif item > a[median]:
-            cmp += 1
-            search_binary(a[median+1, len(a) - 1])
+    return cmp
 
 
 def test_search():
-    assert search_linear([1, 9, 3, 2, 1, 5], 9) == 1
-    assert search_binary(range(20), 10) == 10
+    assert search_linear(["1", "9", "3", "2", "1", "5"], "9") == 1
+    assert search_binary(["1", "1", "2", "3", "5", "9"], "5") == 4
 
 
 if __name__ == "__main__":
