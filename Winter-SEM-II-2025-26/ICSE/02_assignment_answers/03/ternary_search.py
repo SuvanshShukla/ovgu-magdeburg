316532a9690fff2d2b8d731965f9608fa63a57f6
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-05 07:47:09 +0100

    add ternary search template

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/03/ternary_search.py
@@ -0,0 +1,57 @@
+from typing import List
+from random import randint
+import time
+
+
+def binary_search(a: List[int], item: int) -> int | None:
+    """Searches the list for item with binary search. Returns the position of item."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def binary_search_helper(a: List[int], item: int, low: int, high: int) -> int | None:
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def ternary_search(a: List[int], item: int) -> int | None:
+    """Searches the list for item with ternary search. Returns the position of item."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+def time_binary_search_in_s(arr: List[int], item: int) -> float:
+    start = time.time()
+    binary_search(arr, item)
+    return time.time() - start
+
+
+def time_ternary_search_in_s(arr: List[int], item: int) -> float:
+    start = time.time()
+    ternary_search(arr, item)
+    return time.time() - start
+
+
+if __name__ == "__main__":
+    # TODO: Test your binary and ternary search implementation here...
+
+    # These constants should be defined here, don't move them!
+    MAX_VALUE = 50_000_000
+    NUM_ITER = 100_000
+    SEARCH_VALUES = [randint(0, MAX_VALUE - 1) for _ in range(NUM_ITER)]
+    ARRAY = list(range(MAX_VALUE))
+    print(f"Searching {NUM_ITER:_} random values in an array of size {MAX_VALUE:_}")
+
+    time_binary = 0.
+    for value in SEARCH_VALUES:
+        time_binary += time_binary_search_in_s(ARRAY, value)
+
+    time_ternary = 0.
+    for value in SEARCH_VALUES:
+        time_ternary += time_ternary_search_in_s(ARRAY, value)
+
+    print(f"Total binary search time:    {time_binary:.9f} s")
+    print(f"Total ternary search time:   {time_ternary:.9f} s")
+    print(f"Average binary search time:  {time_binary / NUM_ITER:.9f} s")
+    print(f"Average ternary search time: {time_ternary / NUM_ITER:.9f} s")
+
+"""
+TODO: Task 3, compare the algorithms.
+"""
