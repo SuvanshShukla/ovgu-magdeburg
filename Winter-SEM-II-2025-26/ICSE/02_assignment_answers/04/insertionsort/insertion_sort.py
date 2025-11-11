1a8d69167e8f7e96e49df9147d1ae13a82e4ad91
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-11 15:50:30 +0100

    add insertion_sort.py template

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/insertionsort/insertion_sort.py
@@ -0,0 +1,15 @@
+"""
+2.  # TODO
+
+"""
+
+
+def insertion_sort(array: list) -> None:
+    """Sorts `array` with insertionsort inplace."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    array = [3, 2, 4, 1, 5]
+    insertion_sort(array)
+    assert array == [1, 2, 3, 4, 5]
