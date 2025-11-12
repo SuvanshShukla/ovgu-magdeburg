727814df5c866924a8185986d47b53967fddcd51
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-12 08:03:17 +0100

    add answer to task 2 of insertion sort

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/insertionsort/insertion_sort.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/insertionsort/insertion_sort.py
@@ -1,12 +1,23 @@
 """
-2.  # TODO
+2. Insertion sort doesn't swap the items in a list/array arround.
+Instead it takes the item and moves it to the correct location in
+the sorted part of the array. The reason why the 1st figure and
+the animation don't do anything is because this algorithm moves
+the items instead of swapping them. It also assumes that a part
+of the list/array is already sorted.
 
 """
 
 
 def insertion_sort(array: list) -> None:
     """Sorts `array` with insertionsort inplace."""
-    raise NotImplementedError()  # TODO: Add implementation
+    for i in range(1, len(array)):
+        insert_loc = i
+        anchor = array.pop(i)
+        for j in range(i - 1, -1, -1):
+            if array[j] > anchor:
+                insert_loc = j
+        array.insert(insert_loc, anchor)
 
 
 if __name__ == "__main__":
