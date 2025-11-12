2d76cca71adbcee0eacf196c84edd0fba736607f
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-12 08:26:32 +0100

    add working bubble sort code

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/bubblesort/bubble_sort.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/bubblesort/bubble_sort.py
@@ -6,10 +6,15 @@
 
 def bubble_sort(array: list) -> None:
     """Sorts `array` with bubblesort inplace."""
-    raise NotImplementedError()  # TODO: Add implementation
+    n = len(array)
+    for i in range(n - 1):
+        for j in range(0, n - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
 
 
 if __name__ == "__main__":
     array = [3, 2, 4, 1, 5]
     bubble_sort(array)
+    print(array)
     assert array == [1, 2, 3, 4, 5]
