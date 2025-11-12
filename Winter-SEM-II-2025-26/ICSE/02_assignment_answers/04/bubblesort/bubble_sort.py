1ee854066b3b3102d3b8a9791927be691478d87e
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-12 08:03:03 +0100

    add bubble sort template files

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/bubblesort/bubble_sort.py
@@ -0,0 +1,15 @@
+"""
+3.  # TODO
+
+"""
+
+
+def bubble_sort(array: list) -> None:
+    """Sorts `array` with bubblesort inplace."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    array = [3, 2, 4, 1, 5]
+    bubble_sort(array)
+    assert array == [1, 2, 3, 4, 5]
