a14a8df2783779893f08605fb2dfdf03a9b773de
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-12 08:34:43 +0100

    add: bubble sort explanation

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/bubblesort/bubble_sort.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/bubblesort/bubble_sort.py
@@ -1,5 +1,15 @@
 """
-3.  # TODO
+3. The bubble sort algorithm iterates over the entire list
+or array and swaps two adjacent elements if the left element
+is greater than the right element. The first figure generated
+by the animation script shows us that sorting starts from the
+end of the list/array and works its way to the front (i.e. greater
+index to lower index). The second animation shows us how the
+algorithm swaps adjacent elements from left to right and does
+it again and again until it has iterated over the list a
+number of times equivalent to the length of the list/array.
+This means an iteration of adjacent swaps happens for every
+element in the array/list.
 
 """
 
