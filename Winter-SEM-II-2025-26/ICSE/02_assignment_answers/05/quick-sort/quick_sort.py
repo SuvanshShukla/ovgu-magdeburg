8219e24d22b7b22af6c76370256496b3e797d3be
Suvansh Shukla <suvanshshukla@gmail.com>
2025-11-18 16:08:38 +0100

    add: quick sort template

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/quick-sort/quick_sort.py
@@ -0,0 +1,17 @@
+"""
+2.  # TODO
+
+3.  # TODO
+
+"""
+
+def quick_sort(array: list) -> None:
+    """Sorts `array` with quick-sort in-place."""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    # Please include more test cases
+    array = [3, 2, 4, 1, 5]
+    quick_sort(array)
+    assert array == [1, 2, 3, 4, 5]
