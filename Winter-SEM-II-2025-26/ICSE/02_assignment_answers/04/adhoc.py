6ebbf253b1b7432f922de5bd9045d86e9d122506
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-19 12:38:11 +0100

    update: fix recursive solution

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/adhoc.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/04/adhoc.py
@@ -10,8 +10,11 @@ def peasant_mult_iter(x: int, y: int) -> int:
     return sum
 
 
+sum = 0
+
+
 def peasant_mult_rec(x: int, y: int) -> int:
-    sum = 0
+    global sum
 
     if x == 0:
         return sum
@@ -23,12 +26,18 @@ def peasant_mult_rec(x: int, y: int) -> int:
 
 
 if __name__ == "__main__":
+    sum = 0
     assert peasant_mult_iter(55, 10) == 55 * 10
+    sum = 0
     assert peasant_mult_iter(24, 52) == 1248
+    sum = 0
     assert peasant_mult_iter(78, 43) == 78 * 43
 
+    sum = 0
     assert peasant_mult_rec(55, 10) == 55 * 10
+    sum = 0
     assert peasant_mult_rec(24, 52) == 1248
+    sum = 0
     assert peasant_mult_rec(78, 43) == 78 * 43
 
     print("ALL TESTS PASSED")
