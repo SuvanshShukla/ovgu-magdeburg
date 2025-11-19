bd37d18cc66f40394a839b190f9416c9263a6946
Suvansh Shukla <suvanshshukla@gmail.com>
2025-11-19 07:15:23 +0100

    add: merge_sort template

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/merge_sort.py
@@ -0,0 +1,16 @@
+"""
+1.  # TODO
+
+2.  # TODO
+
+"""
+
+def merge_sort(array: list[int]) -> list[int]:
+    """Sorts `array` with merge-sort"""
+    raise NotImplementedError()  # TODO: Add implementation
+
+
+if __name__ == "__main__":
+    array = [3, 2, 4, 1, 5]
+    merge_sort(array)
+    assert array == [1, 2, 3, 4, 5]
