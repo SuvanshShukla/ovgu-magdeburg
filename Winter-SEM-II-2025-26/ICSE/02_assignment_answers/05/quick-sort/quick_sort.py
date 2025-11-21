e2ddf42533d29be1d629b66f6cbc963c45f4da68
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-21 10:48:56 +0100

    update: not in-place quicksort implementation

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/quick-sort/quick_sort.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/quick-sort/quick_sort.py
@@ -5,13 +5,43 @@
 
 """
 
+
 def quick_sort(array: list) -> None:
     """Sorts `array` with quick-sort in-place."""
-    raise NotImplementedError()  # TODO: Add implementation
+    size = len(array)
+    if size < 2:
+        return array
+
+    mid = size // 2
+    pivot = array[mid]
+    less = []
+    equal = []
+    greater = []
+    # i = 0
+    # j = size - 1
+
+    for i in array:
+        if i < pivot:
+            less.append(i)
+        elif i == pivot:
+            equal.append(i)
+        elif i > pivot:
+            greater.append(i)
+            
+    # while i < j:
+    #     if array[i] < pivot:
+    #         less.append(array[i])
+    #     i += 1
+    #     if array[j] >= pivot:
+    #         greater.append(array[j])
+    #     j -= 1
+
+    return quick_sort(less) + equal + quick_sort(greater)
 
 
 if __name__ == "__main__":
     # Please include more test cases
     array = [3, 2, 4, 1, 5]
     quick_sort(array)
+    print(array)
     assert array == [1, 2, 3, 4, 5]
