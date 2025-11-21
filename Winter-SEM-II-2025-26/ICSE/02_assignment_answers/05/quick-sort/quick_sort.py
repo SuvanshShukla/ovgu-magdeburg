4fb8c29acc3f03a5225782a75f9f76b74f1841c0
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-21 15:55:35 +0100

    update: add working in-place quick sort implementation

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/quick-sort/quick_sort.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/05/quick-sort/quick_sort.py
@@ -6,42 +6,36 @@
 """
 
 
+def partition(array, start, end):
+    pivot = array[end]
+    i = start - 1
+    j = 0
+
+    for j in range(start, end):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[end] = array[end], array[i + 1]
+    return i + 1
+
+
+def qsort(array, start, end):
+    if start < end:
+        mid = partition(array, start, end)
+        qsort(array, start, mid - 1)
+        qsort(array, mid + 1, end)
+
+
 def quick_sort(array: list) -> None:
     """Sorts `array` with quick-sort in-place."""
-    size = len(array)
-    if size < 2:
-        return array
-
-    mid = size // 2
-    pivot = array[mid]
-    less = []
-    equal = []
-    greater = []
-    # i = 0
-    # j = size - 1
-
-    for i in array:
-        if i < pivot:
-            less.append(i)
-        elif i == pivot:
-            equal.append(i)
-        elif i > pivot:
-            greater.append(i)
-            
-    # while i < j:
-    #     if array[i] < pivot:
-    #         less.append(array[i])
-    #     i += 1
-    #     if array[j] >= pivot:
-    #         greater.append(array[j])
-    #     j -= 1
-
-    return quick_sort(less) + equal + quick_sort(greater)
+    qsort(array, 0, len(array) - 1)
 
 
 if __name__ == "__main__":
     # Please include more test cases
     array = [3, 2, 4, 1, 5]
     quick_sort(array)
-    print(array)
+    arr2 = [64, 34, 25, 5, 22, 11, 90, 12]
+    quick_sort(arr2)
     assert array == [1, 2, 3, 4, 5]
+    assert arr2 == [5, 11, 12, 22, 25, 34, 64, 90]
