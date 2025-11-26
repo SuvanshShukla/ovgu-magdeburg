dad6634ea8aafd39b638465730cc58616eb0b497
SuvanshShukla <suvanshshukla@gmail.com>
2025-11-26 09:31:05 +0100

    update: add constructor

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/06/clock.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/06/clock.py
@@ -1,4 +1,18 @@
 class Clock:
+    hours: int = 0
+    minutes: int = 0
+
+    def __init__(self, hours: int, minutes: int):
+        if hours > 24:
+            self.hours = hours % 24
+        else:
+            self.hours = hours
+
+        if minutes > 60:
+            self.minutes = minutes % 60
+        else:
+            self.minutes = minutes
+
     def __eq__(self, other: object) -> bool:
         # if self and other are of different, incompatible types, they are not equal
         if not isinstance(other, Clock):  
