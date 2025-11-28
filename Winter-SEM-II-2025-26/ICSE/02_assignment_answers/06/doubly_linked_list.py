f931b55e0dccf2140ee5f272b57aa293ef84a729
Suvansh Shukla <suvanshshukla@gmail.com>
2025-11-28 10:26:07 +0100

    update: working doubly linked list

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/06/doubly_linked_list.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/06/doubly_linked_list.py
@@ -1,119 +1,99 @@
-class DoublyLinkedNode:
-    def __init__(self, item: int = None,
-                 next: "DoublyLinkedNode | None" = None,
-                 prev: "DoublyLinkedNode | None" = None) -> None:
-        self._item = item
-        self._next = next
-        self._prev = prev
-
-    def get_next(self) -> "DoublyLinkedNode | None":
-        return self._next
-
-    def get_item(self) -> int | None:
-        return self._item
-
-    def get_prev(self) -> "DoublyLinkedNode | None":
-        return self._prev
-
-    def set_next(self, next):
-        self._next = next
-
-    def set_prev(self, prev):
-        self._prev = prev
-
-    def set_item(self, item):
-        self._item = item
+class Node:
+    def __init__(self, data, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
 
 
 class DoublyLinkedList:
-    def __init__(self) -> None:
-        self._head = DoublyLinkedNode(next=self)
-        self._tail = DoublyLinkedNode(prev=self)
-        self._size = 0
+    def __init__(self):
+        self.head = Node(0)
+        self.tail = Node(0)
+        self.tail.prev = self.head
+        self.head.next = self.tail
+        self.size = 0
 
-    def __len__(self) -> int:
-        return self._size
+    def add_first(self, data):
+        new = Node(data)
+        head = self.head
+        old = self.head.next
+        new.next = old
+        new.prev = head
+        old.prev = new
+        head.next = new
+        self.size += 1
 
-    def is_empty(self) -> bool:
-        if self._size == 0:
+    def add_last(self, data):
+        new = Node(data)
+        tail = self.tail
+        old = self.tail.prev
+        new.next = tail
+        new.prev = old
+        old.next = new
+        tail.prev = new
+        self.size += 1
+
+    def is_empty(self):
+        if self.size == 0:
             return True
         else:
             return False
 
-    def add_first(self, item) -> None:
-        node = DoublyLinkedNode(item)
-        old_head = self._head
-        node.set_next(old_head)
-        old_head.set_prev(node)
-        self._head = node
-        self._size += 1
-
-    def get_first(self) -> int:
+    def get_first(self):
         if self.is_empty():
             return None
-        else:
-            return self._head.get_item()
+        return self.head.next.data
 
-    def remove_first(self) -> int | None:
+    def get_last(self):
         if self.is_empty():
             return None
-        else:
-            first_item = self._head.get_item()
-            self._head = self._head.get_next()
-            self._size -= 1
-            return first_item
+        return self.tail.prev.data
 
-    def add_last(self, item) -> None:
-        node = DoublyLinkedNode(item)
-        old_tail = self._tail
-        old_tail.set_next(node)
-        node.set_prev(old_tail)
-        self._tail = node
-        self._size += 1
+    def __len__(self):
+        return self.size
 
-    def get_last(self) -> int | None:
-        if self.is_empty():
-            return None
-        return self._tail.get_item()
+    def remove_first(self):
+        first = self.head.next
+        new_first = first.next
+        new_first.prev = self.head
+        self.head.next = new_first
+        self.size -= 1
+        return first.data
 
-    def remove_last(self) -> int | None:
-        if self.is_empty():
-            return None
-        else:
-            item = self._tail.get_item()
-            self._tail = self._tail.get_prev()
-            self._size -= 1
-            return item
+    def remove_last(self):
+        last = self.tail.prev
+        new_last = last.prev
+        new_last.next = self.tail
+        self.tail.prev = new_last
+        self.size -= 1
+        return last.data
 
-    def at(self, index: int) -> int:
-        if index > self._size:
-            raise IndexError
-        elif self.is_empty():
-            return None
-        else:
-            current = self.get_first()
-            i = 0
-            while i < index:
-                current = self._head.get_next()
-                i += 1
-            return current.get_item()
-
-    # def __repr__(self):
-    #     return f"[{self._head.get_item()}]"
+    def at(self, index):
+        start = self.head.next
+        while start is not None and index > 0:
+            start = start.next
+            index -= 1
+        if start is not None and start != self.tail and index == 0:
+            return start.data
+        raise IndexError
 
 
 if __name__ == "__main__":
     ll = DoublyLinkedList()
     ll.add_first(1)
-    assert ll.get_first() == 1
-    ll.add_first(2)
-    assert ll.get_first() == 2
-    assert len(ll) == 2
-    ll.add_last(0)
-    assert ll.get_last() == 0
-    assert ll.remove_first() == 2
-    assert ll.remove_last() == 0
+    ll.add_last(2)
     assert ll.is_empty() is False
-    assert ll.at(0) == 1
-    print("ALL TESTS PASSED.")
+    assert ll.get_first() == 1
+    assert ll.get_last() == 2
+    assert len(ll) == 2
+    assert ll.remove_first() == 1
+    assert len(ll) == 1
+    assert ll.remove_last() == 2
+    assert len(ll) == 0
+    ll.add_first(10)
+    ll.add_first(11)
+    assert ll.at(1) == 10
+    assert DoublyLinkedList().is_empty()
+    assert len(DoublyLinkedList()) == 0
+    assert DoublyLinkedList().get_first() is None
     pass
