41c0cf151acc5fc831377a024d2d988e27a60faf
SuvanshShukla <suvanshshukla@gmail.com>
2025-12-03 10:00:21 +0100

    update: handle empty case

--- Winter-SEM-II-2025-26/ICSE/02_assignment_answers/07/random_queue.py
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/07/random_queue.py
@@ -8,15 +8,16 @@ class RandomQueue(Queue):
 
     def dequeue(self) -> Optional[Any]:
         """Remove and return one random item from the queue (or `None` if the queue is empty)."""
+        if self.is_empty:
+            return None
         raise NotImplementedError()
 
     def sample(self) -> Optional[Any]:
         """Returns a random element from the queue (or `None` if the queue is empty)."""
+        if self.is_empty:
+            return None
         raise NotImplementedError()  # TODO: Implement the method `sample`.
 
 
 if __name__ == "__main__":
     pass
-
-# TODO: Remove all lines, that contain "# TODO:"...
-
