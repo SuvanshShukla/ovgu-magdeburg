389f71258ef906df683ff2200a4598ecbef547e2
Suvansh Shukla <suvanshshukla@gmail.com>
2026-01-07 11:57:55 +0100

    add: Ad Hoc task for ex9

---
+++ Winter-SEM-II-2025-26/ICSE/02_assignment_answers/09/adhoc.py
@@ -0,0 +1,35 @@
+# Ad Hoc task for exercise 9 (07/01/2026)
+
+# We already have a BinaryTree class and now we have to implement
+# is_balanced(self) -> bool this checks whether the given
+# (sub)tree is AVL balanced or not and returns the corresponding bool value
+# and inorder_next(self) -> BinaryTree | None this returns the node that would
+# come next inorder traversal We can assume that the BinaryTree is also
+# a SearchTree If no such successor exists then return None
+
+class BinaryTree:
+    def __init__(self, data: int):
+        pass
+
+    def get_parent(self) -> BinaryTree | None:
+        pass
+
+    def get_left(self) -> BinaryTree | None:
+        pass
+
+    def get_right(self) -> BinaryTree | None:
+        pass
+
+    def get_data(self) -> int:
+        pass
+
+    def height(self) -> int:
+        pass
+
+    def is_balanced(self) -> bool:
+        """This is the function we have to implement"""
+        pass
+
+    def inorder_next(self) -> BinaryTree | None:
+        """This is the second function we have to implement"""
+        pass
