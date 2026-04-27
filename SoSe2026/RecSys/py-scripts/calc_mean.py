ecfeca9a05fbd5da4b3ba7253cba5b9b7e8f959d
Suvansh Shukla <suvanshshukla@gmail.com>
2026-04-27 08:37:33 +0200

    add: mean calculation script

---
+++ SoSe2026/RecSys/py-scripts/calc_mean.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+u1 = [4, 4, 1, 2, 4, 2]
+u2 = [2, 1, 5, 4, 2, 3]
+u3 = [5, 1, 1, 1, 4, 1]
+
+print('mean for u1: ', np.mean(u1))
+print('mean for u2: ', np.mean(u2))
+print('mean for u3: ', np.mean(u3))
